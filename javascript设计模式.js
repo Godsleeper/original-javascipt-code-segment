0d541fa445324b425b4ac2e291a8946811158ade
Godsleeper <nxtw@qq.com>
2016-09-06 20:15:39 +0800

    观察者模式的完整实现

--- javascript设计模式.js
+++ javascript设计模式.js
@@ -40,3 +40,57 @@ button.addEventListener('click',function(){
 })
 
 //发布订阅模式(观察者模式)
+//发布者模型
+var event={
+	clientList:[],//缓存列表
+	listen:function(key,fn){
+		if(!this.clientList[key]{
+			this.clientList[key]=[];//判断某个事件的订阅列表是不是空的，如果是空的，就新建一个列表
+		}
+		this.clientList[key].push(fn);//将订阅的事件存入缓存列表
+	},
+	trigger:function(){
+		var key = Array.prototype.shift.call(arguments);//取出订阅事件类型
+		var fns = this.clientList[key];//找到订阅事件列表
+		if(!fns||fns.length===0){
+			return false;
+		}
+		for(var i=0;i<fns.length;i++){
+			fn.apply(this,arguments)//依次调用订阅事件
+		}
+	},
+	remove:function(key,fn){
+		var fns = this.clientList[key];//取出事件缓存列表
+		if(!fns){
+			return false;
+		}
+		if(!fn){//如果没有传入回调函数，表示取消key对应
+			fns&&(fns.length=0)//清空缓存队列
+		}else{
+			for(var i = fns.length-1;i>=0;i--){
+				var _fn = fns[i];
+				if(_fn===fn){
+					fns.splice(i,1);//删除该函数
+				}
+			}
+		}
+	}
+
+
+}
+//类型装载器（深拷贝）
+var installEvent = function(eventobj){
+	for(var i in event){
+		eventobj[i] = event[i];
+	}
+}
+
+//用例
+var usecase = {};
+installEvent(usecase);
+usecase.listen('event1',function(arguments){
+	//订阅了事件的响应
+})
+usecase.trigger('event1',arguments);
+
+

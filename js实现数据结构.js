34a2458d0bbfb26ad615bf6639b9582054321bea
Godsleeper <nxtw@qq.com>
2016-09-24 19:32:14 +0800

    排序

--- js实现数据结构.js
+++ js实现数据结构.js
@@ -1,23 +1,11 @@
-//**************************************************完美排序类*****************************************
-function ArrayList(){
-	var array=[];
-
-	//插入
-	this.insert = function(item){
-		array.push(item);
-	}
-	//转为字符串
-	this.toString = function(){
-		array.join();
-	}
-	//交换数组下标位置
+//**************************************************快速排序*****************************************
 	var swap=function(array,index1,index2){
 		var aux = array[index1];
 		array[index1]=array[index2];
 		array[index2]=aux;
 	};
 
-	this.quickSort = function(){
+	var quickSort = function(array){
 		quick(array,0,array.length-1);
 	};
 
@@ -50,7 +38,41 @@ function ArrayList(){
 		}
 		return i;
 	}
+//**************************************************快速排序*****************************************
+
+//**************************************************冒泡排序******************************************
+var bublesort = function(array){
+	for(var i=1;i<array.length;i++){
+		for(var j=array.length-1;j>=i;j--){
+			if(array[j-1]>array[j]){
+				var _item=array[j];
+				array[j]=array[j-1];
+				array[j-1]=_item;
+			}
+		}
+	}
 }
 
+//**************************************************冒泡排序******************************************
 
+//**************************************************选择排序******************************************
+var chosesort=function(array){
+	for(var i=0;i<array.length-1;i++){
+		var _item=i;
+		for(var j=i+1;j<array.length;j++){
+			if(array[_item]>array[j]){
+				console.log(_item);
+				_item=j;
+			}
+		}
+		if(i!=_item){
+			var _temp=_item;
+			_item=i;
+			i=_temp;
+		}
+	}
+}
 
+var arr=[1,5,3,2,5,6,2];
+chosesort(arr);
+console.log(arr)
